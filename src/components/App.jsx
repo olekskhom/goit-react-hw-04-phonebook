e666b792c97de7f151d8c2ef183d5343535f0850
olekskhom <oleks.khom@gmail.com>
2023-10-09 13:31:41 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
+import { useEffect, useState } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const localeStorageData = JSON.parse(localStorage.getItem('contacts'));
+    setContacts(() => [...localeStorageData]);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleFilterChange = event => {
+    setFilter(() => event.target.value);
+  };
+
+  const addContact = (name, number, id) => {
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    setContacts(prevState => [...prevState, { name, number, id }]);
+  };
+
+  const filterContacts = () => {
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  };
+
+  const deleteContact = name => {
+    const deleteContacts = contacts.filter(contact => {
+      return contact.name.toLowerCase() !== name.toLowerCase();
+    });
+
+    setContacts(() => [...deleteContacts]);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h2 className={css.title}>Phonebook</h2>
+      <ContactForm addContact={addContact} />
+      <h2 className={css.title}>Contacts</h2>
+      <Filter filter={filter} hendleFilterChange={handleFilterChange} />
+      <ContactList contacts={filterContacts()} deleteContact={deleteContact} />
     </div>
   );
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import css from './ContactForm.module.css';
+
+export const ContactForm = ({ addContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = event => setName(() => event.target.value);
+  const handleNumberChange = event => setNumber(() => event.target.value);
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    addContact(name, number, nanoid());
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleFormSubmit}>
+      <label className={css.label} htmlFor="">
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleNameChange}
+        />
+      </label>
+      <label className={css.label} htmlFor="">
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleNumberChange}
+        />
+      </label>
+      <input className={css.btn} type="submit" value="Add contact" />
+    </form>
+  );
+};
+
+ContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
